2b6d9abc1aa746991c7c600c1b8d7aac4fc196eb
nofilenamed <nofilenamed@gmail.com>
2021-05-03 15:20:35 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("slapi.stardew.xpmultiplier")]
+[assembly: AssemblyProduct("smapi.stardew.xpmultiplier")]
 [assembly: AssemblyCopyright("Copyright ©  2021 nofilenamed")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
